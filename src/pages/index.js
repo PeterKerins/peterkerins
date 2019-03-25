b39c31467c8d9c2d21d18339a64f32c6eccc9f15
Peter Kerins <pete.kerins@gmail.com>
2019-03-25 13:56:15 +0000

    remove banner from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
-import Layout from '../components/homeLayout'
+import Layout from '../components/layout'
 import Work from '../sections/work'
 import Services from '../sections/services'
 import Contact from '../sections/contact'
